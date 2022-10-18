2383df006cd0850b7c92a628aad43166d6f37669
Finn <fjlvanmontfort@gmail.com>
2022-10-18 10:18:01 +0100

    Login page not working

--- flasktest.py
+++ flasktest.py
@@ -1,14 +1,30 @@
-from urllib import response
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect
 import requests
 import json
 
 app = Flask(__name__)
+username = ""
 
+def login(username, password):
+    if (username == "test") and (password =="test"):
+            username = "test"
+            return redirect("/welcome")
+    return render_template("index.html")
+
+@app.route("/welcome",)
+def welcome():
+    return render_template("welcome.html",username=username)
+
+@app.route("/login", methods =['GET','POST'])
+def login_page():
+    if request.method=='POST':
+        username = request.form['username']
+        password = request.form['password']
+    login(username, password)
+    return render_template("login.html")
 
 @app.route("/")
-def index():
-    x = "Hello world"
-    return render_template("index.html", x=x)
+def home():
+    return render_template("index.html")
 
 app.run(host="0.0.0.0",port=80)
