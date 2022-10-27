75cfd7e6874d63f6dca0ec6241711eb5e0badfba
Finn <fjlvanmontfort@gmail.com>
2022-10-27 21:00:05 +0100

    improve login with sessions

--- flasktest.py
+++ flasktest.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
-user = "default"
+
+app.secret_key = "Hello fellow programmer"
 
 def login(username, password):
     if (username == "finn") and (password=="test"):
@@ -9,35 +10,52 @@ def login(username, password):
     else:
         return False
 
-#defines what happens at the route /welcome this allows us to welcome the user
+'''
+defines what happens at the route /welcome this allows us to welcome the user
+'''
 @app.route("/welcome")
 def welcome():
-    if user != "default":
-        return render_template("welcome.html",username=user)
+    if "user" in session:
+        user = session["user"]
+        return render_template("welcome.html")
     else:
         return redirect(url_for('login_page'))
 
-#defines what happens at the route /login this allows the user to login and us to redirect them to the welcome page
+
+'''
+defines what happens at the route /login this allows the user to login and us to redirect them to the welcome page
+'''
 @app.route("/login", methods =['GET','POST'])
 def login_page():
     if request.method=='POST':
-        username = request.form['username']
-        password = request.form['password']
-        login_failed = not login(username, password)
-        if not login_failed:
-            global user 
-            user = username
+        user = request.form['username']
+        pw = request.form['password']
+        session["user"] = user
+        login_failed = not login(user, pw)
+        if not login_failed or "user" in session:
             return redirect(url_for('welcome'))
         else:
-            return render_template("login.html", login_f = True)
-    return render_template("login.html", login_f = True)
+            return render_template("login.html")
+    return render_template("login.html")
 
-#defines what happens at the route /sign up, this allows the user to create an account
+'''
+defines what happens when you logout
+'''
+@app.route("/logout")
+def logout():
+    session.pop("user",None)
+    return redirect(url_for("login_page"))
+
+'''
+defines what happens at the route /sign up, this allows the user to create an account
+'''
 @app.route("/signup")
 def signup():
     return "signed up"
 
-#defines what happens at the route at the defalt site, gateway to the rest of the website
+'''
+defines what happens at the route at the defalt site, gateway to the rest of the website
+'''
 @app.route("/")
 def home():
     return render_template("index.html")
